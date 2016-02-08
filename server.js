5977e4bccbe7572c4a72d168a78806a301ff702a
OldAndInYourWay <metroidman12@gmail.com>
2016-02-08 10:04:23 -0500

    obscure-beyond-33201

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/public"));
 
